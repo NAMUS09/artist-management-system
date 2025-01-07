7c8836984b3765649e8c3281281c8d2c1024622f
Suman Shrestha <suman.shrestha@logiclync.com>
2025-01-07 18:13:10 +0545

    Enhance user upsert route to update access token and cookies on role change for logged-in users

--- app/api/user/upsert/route.ts
+++ app/api/user/upsert/route.ts
@@ -8,11 +8,16 @@ import { CreateUser, createUserSchema } from "@/app/schemas/userSchema";
 import { hash } from "@/app/utils/common";
 import CustomError from "@/app/utils/customError";
 import roleAsyncHandler from "@/app/utils/roleAsyncHandler";
+import { generateToken } from "@/app/utils/token";
 import { validateRequestBody } from "@/app/utils/validateBody";
+import { getCurrentUser } from "@/lib/auth";
 import { User } from "@/lib/interface";
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export const POST = roleAsyncHandler(["super_admin"], async (req: Request) => {
+  const cookieStore = await cookies();
+  const loggedinUser = await getCurrentUser();
   const body = (await req.json()) as CreateUser;
 
   const [validationError, validatedFields] = validateRequestBody(
@@ -39,12 +44,42 @@ export const POST = roleAsyncHandler(["super_admin"], async (req: Request) => {
       throw new CustomError("User not found");
     }
     if (password) {
+      console.log(password);
       body.password = await hash(password);
     } else {
       body.password = user.password;
     }
 
     const rows = await updateUserById(id, { ...user, ...prepareUser } as User);
+
+    // check role has updated, and if smae user is logged in
+    if (user.role !== prepareUser.role && loggedinUser?.email === user.email) {
+      console.log("role chngad");
+      // role changed
+      // create and  update cookies /token
+      // safe to generate token
+      const { token, expires } = await generateToken({
+        id: user.id,
+        email: user.email!,
+        name: `${user.first_name} ${user.last_name}`,
+        role: user.role,
+      });
+
+      // update user token
+      const { id, ...rest } = user;
+      await updateUserById(id, {
+        ...rest,
+        accessToken: token,
+        expires,
+      } as unknown as User);
+
+      cookieStore.set({
+        name: "accessToken",
+        value: token,
+        httpOnly: true,
+        path: "/",
+      });
+    }
 
     return NextResponse.json({
       status: 200,
